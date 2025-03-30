6b967870fa5b109972ab46bc3acf1e7e509923f6
Gaetan Wcz <gaetan.wichlacz@ext.adeo.com>
2025-03-30 23:17:58 +0200

    feat(): init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://nomutilisateur.github.io/', // Remplace par ton URL
+  base: '/',
+  output: 'static',
+});
